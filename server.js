5317d7780df05884556041279aecb32d328e06b6
gccunanan <gccunanan@gmail.com>
2019-09-18 20:29:38 -0700

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path =require('path');
 
-const songs = require('./routes/api/songs');
+const songs = require('./routes/api/Songs.js')
 
 const app = express();
 
