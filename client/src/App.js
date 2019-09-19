d84884d4d4653d9debe6dcde0a8d42f58a6819ef
gccunanan <gccunanan@gmail.com>
2019-09-19 01:00:26 -0700

    client side authentications with tokens and error;redux

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/authActions';
 
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
   render() {
     return (
       <Router>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import songReducer from './songReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
+
 
 export default combineReducers({
-    song: songReducer
+    song: songReducer,
+    error: errorReducer,
+    auth: authReducer
 });
